ee63b04bccf66f74e75e604f69a2caa7e5b569c2
Samir-Lora <slora@wawand.co>
2024-04-15 21:23:38 -0500

    add navigation bar

--- app/src/main/java/com/example/iubegresados/data/model/UserModel.kt
+++ app/src/main/java/com/example/iubegresados/data/model/UserModel.kt
@@ -1,30 +1,30 @@
-//package com.example.iubegresados.data.model
-//
-//import androidx.room.ColumnInfo
-//import androidx.room.Entity
-//import androidx.room.PrimaryKey
-//
-//data class UserModel(
-//
-//    val id: String,
-//    val firstname: String,
-//    val lastname: String,
-//
-//    @ColumnInfo(name = "email")
-//    val email: String,
-//
-//    @ColumnInfo(name = "cellphone")
-//    val cellphone: String,
-//
-//    @ColumnInfo(name = "address")
-//    val address: String,
-//
-//    @ColumnInfo(name = "password")
-//    val password: String,
-//
-//    @ColumnInfo(name = "status")
-//    val status: Boolean,
-//
-//    @ColumnInfo(name = "creationdate")
-//    val creationdate: String
-//)
+package com.example.iubegresados.data.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+data class UserModel(
+
+    val id: String,
+    val firstname: String,
+    val lastname: String,
+
+    @ColumnInfo(name = "email")
+    val email: String,
+
+    @ColumnInfo(name = "cellphone")
+    val cellphone: String,
+
+    @ColumnInfo(name = "address")
+    val address: String,
+
+    @ColumnInfo(name = "password")
+    val password: String,
+
+    @ColumnInfo(name = "status")
+    val status: Boolean,
+
+    @ColumnInfo(name = "creationdate")
+    val creationdate: String
+)
--- app/src/main/java/com/example/iubegresados/ui/activities/Home.kt
+++ app/src/main/java/com/example/iubegresados/ui/activities/Home.kt
@@ -1,35 +1,48 @@
 package com.example.iubegresados.ui.activities
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import com.example.iubegresados.R
 import com.example.iubegresados.adapters.JobOfferAdapter
+import com.example.iubegresados.ui.fragments.HomeFragment
+import com.example.iubegresados.ui.fragments.UserFragment
 import com.example.iubegresados.ui.viewModels.JobOfferViewModel
-
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class Home : AppCompatActivity() {
-    private lateinit var jobOfferViewModel: JobOfferViewModel
     private lateinit var recyclerView: RecyclerView
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
 
-        jobOfferViewModel = ViewModelProvider(this).get(JobOfferViewModel::class.java)
-
-        jobOfferViewModel.getJobOffers()
-
-        jobOfferViewModel.jobOffers.observe(this) { jobOffers ->
-            val adapter = JobOfferAdapter(jobOffers, jobOfferViewModel)
-            recyclerView.adapter = adapter
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home_item -> {
+                    replaceFragment(HomeFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.profile_item -> {
+                    replaceFragment(UserFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+            }
+            false
         }
-
-
-
     }
 
-    @SuppressLint("MissingSuperCall")
-    override fun onBackPressed() {}
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainerView, fragment)
+            .commit()
+    }
+
+    override fun onBackPressed() {
+        // Overriding onBackPressed to do nothing
+    }
 }
