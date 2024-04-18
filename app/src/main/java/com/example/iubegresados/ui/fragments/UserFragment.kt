d14a9705b9c9ee724f79c400146bb214f38ac400
juanevilla <juan.evilla@hotmail.com>
2024-04-17 23:44:07 -0500

    fetch

--- app/src/main/java/com/example/iubegresados/ui/fragments/UserFragment.kt
+++ app/src/main/java/com/example/iubegresados/ui/fragments/UserFragment.kt
@@ -1,24 +1,42 @@
 package com.example.iubegresados.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.example.iubegresados.R
+import com.example.iubegresados.data.model.SessionManager
+import com.example.iubegresados.ui.viewModels.UserViewModel
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+
 class UserFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private lateinit var userViewModel: UserViewModel
+    private lateinit var sessionManager: SessionManager
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        userViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+
+        val userid = sessionManager.fetchUser()?.userid
+
+        // Verifica que el userid no sea nulo antes de llamar a getUserBy()
+        if (!userid.isNullOrEmpty()) {
+            userViewModel.getUserBy(userid)
+            userViewModel.userInfo.observe(viewLifecycleOwner) { userInfo ->
+                Log.d("UserFragment", "Usuario: $userInfo")
+            }
+        } else {
+            Log.e("UserFragment", "No se pudo obtener el userid")
+        }
+
+        userViewModel.getUserBy("")
+        userViewModel.userInfo.observe(viewLifecycleOwner) { userInfo ->
+            Log.d("HomeFragment", "Usuario: $userInfo")
         }
     }
 
@@ -30,23 +48,4 @@ class UserFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_user, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment UserFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            UserFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
